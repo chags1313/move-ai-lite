ccae893111f123cfda28d84228977fb6325a24c1
Cole Hagen <61998370+chags1313@users.noreply.github.com>
2023-06-19 13:52:12 -0400

    Update ms.py

--- ms.py
+++ ms.py
@@ -584,7 +584,7 @@ if video_file is not None:
         joint_line_plot = create_joint_line_plot(df_joint_angles, jnt, slide = int(st.session_state['slide_value'] * fps), color_discrete_map=color_discrete_map)
         # Create joint velocity plot
         joint_velocity_plot = create_joint_velocity_plot(df_joint_angles, jnt, slide = int(st.session_state['slide_value'] * fps), color_discrete_map=color_discrete_map)
-        pl1.plotly_chart(joint_velocity_plot, use_container_width=True, config= {'displaylogo': False})
+        st.plotly_chart(joint_velocity_plot, use_container_width=True, config= {'displaylogo': False})
         st.plotly_chart(joint_line_plot, use_container_width=True, config= {'displaylogo': False})
         if play:
             # Iterate over the images
@@ -613,7 +613,7 @@ if video_file is not None:
                 time.sleep(1/30)
         #st.plotly_chart(imag, use_container_width=True, config= {'displaylogo': False})
         #cnr.image(st.session_state.key_arr[int(st.session_state['slide_value'] * fps)], channels='BGR')
-        cnr.video(st.session_state.key_arr, start_time = 5)
+        st.video(st.session_state.key_arr, start_time = 5)
         st.write("_____")
         st.write("_____")
 
