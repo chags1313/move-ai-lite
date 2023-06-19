74c32c657756fc3e51a3fddadbf705c360abcf8f
Cole Hagen <61998370+chags1313@users.noreply.github.com>
2023-06-19 14:14:22 -0400

    Update ms.py

--- ms.py
+++ ms.py
@@ -394,7 +394,7 @@ def create_joint_velocity_plot(df_joint_angles, jnt, slide, color_discrete_map, 
     joint_velocity_plot.update_layout(height = height, hovermode="x", showlegend = False, title = 'Velocity (degrees/second)')
     joint_velocity_plot.update_xaxes(tickformat="%H:%M:%S", title = 'Seconds (HH:MM:SS)')
     joint_velocity_plot.update_yaxes(title = None)
-    joint_velocity_plot.add_hline(x = df_joint_angles['time'].iloc[slide], line_color = 'grey')
+    joint_velocity_plot.add_vline(x = df_joint_angles['time'].iloc[slide], line_color = 'grey')
     return joint_velocity_plot
 
 #######################################
